9120414889b461b5da654bb10b7540166f5af818
akash99-code <53252168+akash99-code@users.noreply.github.com>
2021-01-04 12:11:44 +0530

    Add files via upload

---
+++ bcaTT.js
@@ -0,0 +1,20 @@
+
+function ecom(){
+    window.open("https://meet.google.com/upw-gmbs-hxw");
+}
+function awr(){
+    window.open("https://meet.google.com/nme-xdww-cnm");
+}
+
+function microp(){
+    window.open("https://meet.google.com/cvo-rqdq-irg");
+}
+function javalab(){
+    window.open("https://meet.google.com/cvo-rqdq-irg");
+ }
+ function syssoft(){
+    alert("netflix time!");
+ }
+ function mc(){
+    alert("go back to sleep bro!!");
+ }
